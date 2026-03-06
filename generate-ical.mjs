f50ce61f687e8a661b982c05922bc4157867d0d3
Nikolai Romanov <kolya.rm@gmail.com>
2026-03-06 22:39:12 +0100

    Implemented the IcalGenerator print function writing the days.ics file calendar structure.

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,9 +1,9 @@
- import { IcalGEnerator } from "./ical-generator.mjs";
+import { IcalGenerator } from "./ical-generator.mjs";
 
+let generator = new IcalGenerator();
 
- let generator = new IcalGEnerator();
+generator.setStartMonth(2025, 0);
+generator.setLastMonth(2026, 11);
 
- generator.setStartMonth(2025, 0);
- generator.setLastMonth(2026, 11);
-
- generator.generateCommemorableDays();
+generator.collect();
+generator.print();
--- ical-generator.mjs
+++ ical-generator.mjs
@@ -1,27 +1,57 @@
+import { writeFileSync, appendFileSync} from "node:fs";
 import { CalendarPage } from "./calendar-page.mjs";
 
+const FILE_NAME = "days.ics";
 
-export class IcalGEnerator {
+const CALENDAR_BEGIN_STRING = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Migracode BCN//Commemorable Calendar 1.0//EN\nCALSCALE:GREGORIAN\n";
+const CALENDAR_END_STRING = "END:VCALENDAR\n";
+const DAY_START_STRING = "BEGIN:VEVENT\n";
+const DAY_END_STRING = "END:VEVENT\n";
+
+export class IcalGenerator {
+
   startMonth;
   lastMonth;
   commemorableDays = [];
 
-
   //region interface
-  generateCommemorableDays() {
+  collect() {
     let currentMonth = new CalendarPage();
-    currentMonth.updateYearMonth(this.startMonth.getFullYear(),this.startMonth.getMonth());
+    currentMonth.updateYearMonth(
+      this.startMonth.getFullYear(),
+      this.startMonth.getMonth(),
+    );
 
     this.commemorableDays.length = 0;
     while (currentMonth.getTime() <= this.lastMonth.getTime()) {
-      this.commemorableDays.push(...currentMonth.getDays().filter(day => day.description));
+      this.commemorableDays.push(
+        ...currentMonth.getDays().filter((day) => day.description),
+      );
       currentMonth.changeMonth(1);
     }
 
     console.log(this.commemorableDays);
   }
-  //endregion
 
+  print() {
+    try {
+      writeFileSync(FILE_NAME, CALENDAR_BEGIN_STRING);
+      this.printDays();
+      appendFileSync(FILE_NAME, CALENDAR_END_STRING);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  printDays() {
+    try {
+      appendFileSync(FILE_NAME, DAY_START_STRING);
+      appendFileSync(FILE_NAME, DAY_END_STRING);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  //endregion
 
   //region getters/setters
   setStartMonth(year, month) {
